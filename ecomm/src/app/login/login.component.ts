59d359881ededaf51fca637b6a08450c415bd3f7
Roy <rott.9954@gmail.com>
2021-10-02 17:27:41 -0500

    segundo, sanchezGarcia, validaLogin

--- ecomm/src/app/login/login.component.ts
+++ ecomm/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+   user = '';
+   pass = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  login(){
+    if("user" == this.user && this.pass == "pass"){
+      Swal.fire({
+        title: 'BIEN!',
+        text: 'El formulario funciona correctamente!',
+        icon: 'success',
+        confirmButtonText: 'Cool'
+      })
+    }else{
+      Swal.fire({
+        title:"MAL!",
+        text: "La contraseña o el usuario están mal",
+        icon: "error",
+        confirmButtonText: "Ok"
+      })
+    }
+  }
 }
